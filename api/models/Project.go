ecc752dc3826c51ebb874f1868930c34dde6e4c5
dgikonyo <denniskimani05@gmail.com>
2023-05-23 22:03:15 +0300

    updates on project status

--- api/models/Project.go
+++ api/models/Project.go
@@ -1,5 +1,29 @@
 package models
 
 import (
-	
+	"errors"
+	"html"
+	"strings"
+	"time"
+
+	"github.com/jinzhu/gorm"
+	"github.com/google/uuid"
 )
+
+type Project struct {
+	gorm.Model
+	id uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primary_key" json:"id,omitempty"`
+	projectName string `gorm:"size:100;not null;unique" json:"projectName"`
+	videoUrl string `json:"videoUrl"`
+	photoUrl string `json:"photoUrl"`
+	projectDescription string `gorm:"size:255;not null" json:"projectDescription"`
+	projectLocation string `gorm:"size:255;not null" json:"projectLocation"`
+	startDate *time.Time `json:"startDate"`
+	endDate *time.Time `json:"endDate"`
+	goal int `gorm:"default:0" json:"goal"`
+	pledged int `gorm:"default:0" json:"pledged"`
+	investors int `gorm:"default:0" json:"investors"`
+	projectStatusId
+	organizationId 
+	userId
+}
